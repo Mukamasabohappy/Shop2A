2d0bab89542fecc32d13cf757349d89ae5d5f30c
Mukamasabohappy <your_email@example.com>
2025-04-02 11:11:42 +0100

    new changes

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react-vendor'; // Separate React-related dependencies
-            }
-            return 'vendor'; // Other third-party dependencies
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 600, // Adjust if needed (not recommended long-term)
-  },
-});
+})
