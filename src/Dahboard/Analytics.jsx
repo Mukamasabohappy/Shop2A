42eae04498a065116a9b339b1e81ad5fb7cb65da
Mukamasabohappy <your_email@example.com>
2025-04-02 11:05:07 +0100

    new changes

--- src/Dahboard/Analytics.jsx
+++ src/Dahboard/Analytics.jsx
@@ -205,6 +205,7 @@ const ClothingStoreAnalytics = () => {
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Avg Price</th>
                 </tr>
               </thead>
+              
               <tbody className="bg-white divide-y divide-gray-200">
                 {topProducts.map((product, index) => (
                   <tr key={index}>
